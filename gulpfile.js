4231926078684483a6238f33b5cd778dea99ba1d
Harold Mantilla <hacodeml@gmail.com>
2019-03-29 06:14:04 -0500

    login.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
@@ -11,9 +12,16 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/assets/styles'));
+    .pipe(gulp.dest('./dist/assets/styles'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('sass:watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
   gulp.watch('./src/assets/styles/**/*.scss', gulp.series('sass'));
+  gulp.watch('./*.html').on('change', browserSync.reload);
 });
