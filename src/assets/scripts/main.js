1be376ce5b4fa83556df9e07ffd0860ec20e66ec
Harold Mantilla <hacodeml@gmail.com>
2019-04-01 06:47:08 -0500

    curriculum.html

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,2 +1,11 @@
-var d = document.getElementById("div1");
-d.className += " otherclass";
+
+let cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    console.log(card);
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('is-flipped');
+    });
+});
+
+
